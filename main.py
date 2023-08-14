584dbbc8552c4904fc7f865513b88c171a73d09d
Keith Fernandes <keith.fernandes@fafadiatech.com>
2023-08-14 15:22:00 +0530

    remove comments

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ class demo:
 		self.msg=msg
 	def __str__(self):
 		return f"Hello_{self.msg}"
-<<<<<<< HEAD
 	def addition(self):
 		return 3+3
-=======
->>>>>>> eb0bcafe935da8f798036e5fefc736713ee0a236
 	def get_random()->int:
 		return random.randrange(1,10)
 
