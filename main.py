067b27f1f521a300db695dff09a5aebaaf5b872a
Keith Fernandes <keith.fernandes@fafadiatech.com>
2023-08-14 15:04:34 +0530

    updated main and added wrapper in utils.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 class demo:
 	def __init__(self,msg):
 		self.msg=msg
@@ -5,6 +6,8 @@ class demo:
 		return f"Hello_{self.msg}"
 	def addition(self):
 		return 3+3
+	def get_random()->int:
+		return random.randrange(1,10)
 
 a=demo(msg="KF")
 print(a)
